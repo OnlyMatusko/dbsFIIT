c1a6670f77995088dee2f1c334b7f0441e26d04f
OnlyMatusko <matusmajer99@gmail.com>
2021-02-28 22:53:00 +0100

    commit9

--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,7 @@ def index(request):
     return HttpResponse("Uvodná stránka kde nič nieje")
 
 def time(request):
-    cursor.execute("SELECT date_trunc('second', current_timestamp -pg_postmaster_start_time()) as uptime")
+    query = "SELECT date_trunc('second', current_timestamp -pg_postmaster_start_time()) as uptime;"
+    cursor.execute(query)
     time = cursor.fetchone()
-    return HttpResponse(json.dump(time), content_type='application/json')
+    return HttpResponse(time, content_type='application/json')
