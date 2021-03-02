a0d786b4b0aa2052150157525c32e8f1387fba8d
OnlyMatusko <matusmajer99@gmail.com>
2021-03-02 11:05:29 +0100

    finalfinal1

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('v1/health', views.time, name='time'),
-    path('v2/health', views.time2, name='time2'),
+    path('v1/health', views.time2, name='time2'),
+    path('v2/health', views.time, name='time'),
 ]
