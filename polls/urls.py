cf4275a07af982f0d4a63e9afe42fa8d7d630dec
OnlyMatusko <matusmajer99@gmail.com>
2021-03-02 08:26:13 +0100

    try1

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,5 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('v1/health', views.time, name='time'),
-
+    path('v2/health', views.time2, name='time2'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -13,3 +13,12 @@ def time(request):
         cursor.execute(query)
         time = cursor.fetchone()
     return HttpResponse(str(time[0]), content_type='application/json')
+
+def time2(request):
+    query = "SELECT date_trunc('second', current_timestamp -pg_postmaster_start_time()) as uptime;"
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        time = cursor.fetchone()
+        curr_time= {"psql" : {"uptime": str(time[0])}}
+        time_j= json.dump(curr_time)
+    return HttpResponse(time_j)
